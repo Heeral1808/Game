f71ad4cfe3d658f72e03ceb9b327ebd596264be6
Heeral1808 <72649074+Heeral1808@users.noreply.github.com>
2020-12-01 15:38:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,65 +1,63 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
-  var wall, thickness;
-  var bullet,speed, weight;
-  
-  function setup() {
-    createCanvas(1600, 400);
-  
-    speed=random(223,321)
-    weight=random(30,52)
-  
-  
-    thickness=random(22,83)
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
 
-    
-      bullet=createSprite(50, 200, 50,5);  
-      bullet.velocityX = speed;
-      bullet.shapeColor=color(255);
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
 
-      
-      wall=createSprite(1200, 200, thickness, height/2);  
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5, {restitution:0.8});
+	Matter.Body.setStatic(packageBody, true);
+	World.add(world, packageBody);
+	
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	 World.add(world, ground);
+
+	 
+	Engine.run(engine);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
   
-      wall.shapeColor=color(230,230,230);
-    
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+  keyPressed();
+ 
+ 
+}
+
+function keyPressed() {
+ if (keyCode == DOWN_ARROW) {
+	Matter.Body.setStatic(packageBody, false);
+	
   }
-  
-  
-  function draw() {
-    background(0);  
-    if(hasCollided(bullet, wall))
-    {
-      bullet.velocityX=0;
-      var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
-  
-      
-    if(damage>10)
-    {
-      wall.shapeColor=color(255,0,0);
-      
-    }
-  
-    
-  
-    if(damage<10)
-    {
-      wall.shapeColor=color(0,255,0);
-    }
-    
-    }
-  
-  
-    drawSprites();
-   
-  }
-  
-  
-  function hasCollided(lbullet, lwall)
-  {
-    bulletRightEdge=lbullet.x +lbullet.width;
-    wallLeftEdge=lwall.x;
-    if (bulletRightEdge>=wallLeftEdge)
-    {
-      return true
-    }
-    return false;
-  }
+}
+
