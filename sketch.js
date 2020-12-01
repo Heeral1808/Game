50c1907a23882ef9f9f7bb68f34d7311da16ef5e
Heeral1808 <72649074+Heeral1808@users.noreply.github.com>
2020-12-01 16:11:06 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -27,40 +27,58 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
 
-
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5, {restitution:0.8});
+	Matter.Body.setStatic(packageBody, true);
 	World.add(world, packageBody);
 	
-
-	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
+	 World.add(world, ground);
 
+	 boxPosition=width/2-100
+ 	boxY=610;
+
+
+	 box1= createSprite(410,650,200, 20) 
+	 box1.shapeColor = "red";
+
+	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);
+
+	 box2 = createSprite(300,610,20, 100)
+	 box2.shapeColor = "red";
+
+	 boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+	 box3 = createSprite(500, 610, 20, 100)
+	 box3.shapeColor = "red";
+
+	 boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
 
 	Engine.run(engine);
-  
 }
 
 
 function draw() {
   rectMode(CENTER);
   background(0);
+  
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
   drawSprites();
+  keyPressed();
+ 
  
 }
 
 function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-
-    
+ if (keyCode == DOWN_ARROW) {
+	Matter.Body.setStatic(packageBody, false);
+	
   }
 }
-
-
 
